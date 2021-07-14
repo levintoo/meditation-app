d17b07eb61a5607fbff319102120f1a31b787c3c
levintoo <tooklevin@gmail.com>
2021-07-14 13:35:41 +0300

    beginning of play circle anime

--- app.js
+++ app.js
@@ -1,46 +1,49 @@
 const app = () => {
-    const song = document.querySelector('.song');
-    const play = document.querySelector('.play');
-    const outline = document.querySelector('.moving-outline circle');
-    const video = document.querySelector('.vid-container video');
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
 
-    //sounds
-    const sounds = document.querySelectorAll('.sound-picker button');
-    //time display
-    const timeDisplay = document.querySelector('.time-dislay');
-    //get lengt of outline
-    const outlineLength = outline.getTotalLength();
-    //duration
-    let fakeDuration = 600;
+  //sounds
+  const sounds = document.querySelectorAll(".sound-picker button");
+  //time display
+  const timeDisplay = document.querySelector(".time-dislay");
+  //get lengt of outline
+  const outlineLength = outline.getTotalLength();
+  //duration
+  let fakeDuration = 600;
 
-    outline.style.strokeDasharray = outlineLength;
-    outline.style.strokeDashoffset = outlineLength;
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
 
-
-    //play sound
-    play.addEventListener('click', () => {
-        checkPlaying(song);
-    });
-    //function to play and stop music
-    const checkPlaying = song => {
-        if (song.paused) {
-            song.play();
-            video.play();
-            play.src = './svg/pause.svg';
-        } else {
-            song.pause();
-            video.pause();
-            play.src = './svg/play.svg';
-        }
+  //play sound
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+  //function to play and stop music
+  const checkPlaying = (song) => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "./svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "./svg/play.svg";
     }
+  };
+  //animate the circle
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
     //animate the circle
-    song.ontimeupdate = () => {
-        let curretTime = song.curretTime;
-        let elapsed = fakeDuration - curretTime;
-        let seconds = Math.floor(elapsed % 60);
-        let minutes = Math.floor(elapsed / 60);
-    }
-    
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+
+
+  };
 };
 
 app();
