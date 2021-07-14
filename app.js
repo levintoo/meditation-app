75f88b165b9b8612cc17f4ecd647a1d961507e17
levintoo <tooklevin@gmail.com>
2021-07-14 12:47:24 +0300

    importing stuff

---
+++ app.js
@@ -0,0 +1,21 @@
+const app = () => {
+   const song = document.querySelector('.song');
+   const play = document.querySelector('.play');
+   const outline = document.querySelector('.moving-outline circle');
+   const video = document.querySelector('.vid-container video');
+
+//sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+    //time display
+    const timeDisplay = document.querySelector('.time-dislay');
+    //get lengt of outline
+    const outlineLength = outline.getTotalLength();
+    //duration
+    let fakeDuration = 600;
+
+    outline.style.strokeDasharray = 100;
+
+
+}
+
+app();
