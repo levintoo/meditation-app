c03c31aa8b6913dfaec0e26c86a23d8d405a28fa
levintoo <tooklevin@gmail.com>
2021-07-14 13:11:24 +0300

    play vid

--- app.js
+++ app.js
@@ -19,8 +19,20 @@ const app = () => {
 
     //play sound
     play.addEventListener('click', ()=>{
-        song.play();
+        checkPlaying(song);
     });
+    //function to play and stop music
+    const checkPlaying = song =>{
+        if(song.paused){
+            song.play();
+            video.play();
+            play.src = './svg.pause.svg';
+        }else{
+            song.pause();
+            video.pause();
+            play.src = './svg.play.svg';
+        }
+    }
 };
 
 app();
