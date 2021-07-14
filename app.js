41c39f6577543893bdd941f2bb0b97c984fada5e
levintoo <tooklevin@gmail.com>
2021-07-14 21:40:55 +0300

    html fix

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ const app = () => {
   outline.style.strokeDasharray = outlineLength;
   outline.style.strokeDashoffset = outlineLength;
 
+  //pick different sound
+  sounds.forEach(sound => {
+    sound.addEventListener("click", function() {
+      song.src = this.getAttribute("data-sound");
+      video.src = this.getAttribute("data-video");
+      checkPlaying(song);
+    });
+  });
+
   //play sound
   play.addEventListener("click", () => {
     checkPlaying(song);
